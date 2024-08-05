5b0091cd3953e70ad4285f31cceec75c563d4f0a
Andreas Wiese <163132137+SasuGamerHD@users.noreply.github.com>
2024-08-05 22:47:24 +0200

    Life

---
+++ run.sh
@@ -0,0 +1,11 @@
+create human = new human;
+run exist.exe;
+day.night.full(program);
+exist.timeout.date=*/2065;
+if exist.timeout=0 {
+  human.exist.end();
+}
+else {
+  human.exist(program);
+}
+#endofcode
